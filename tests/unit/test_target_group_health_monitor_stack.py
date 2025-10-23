e06d5087183ac56384199dbc7f976965bddd4b19
Thomas Kosciuch <thomas@qwealth.com>
2025-10-23 14:40:48 -0400

    Initial commit

---
+++ tests/unit/test_target_group_health_monitor_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from target_group_health_monitor.target_group_health_monitor_stack import TargetGroupHealthMonitorStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in target_group_health_monitor/target_group_health_monitor_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = TargetGroupHealthMonitorStack(app, "target-group-health-monitor")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
